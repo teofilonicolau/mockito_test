63ccaa7014ef6f58d4eba2e2b0219a59cfe77d8b
teofilo_nicolau <teofilonicolau157@gmail.com>
2024-06-11 10:48:03 -0300

    alteraçoes

---
+++ src/test/java/me/dio/mockito/exemplos/GeradorDeNumerosTest.java
@@ -0,0 +1,18 @@
+package me.dio.mockito.exemplos;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class GeradorDeNumerosTest {
+
+    @Test
+    void testeDeGeracaoComTamanhoDefinido() {
+        MockedStatic<GeradorDeNumeros> mockedStatic = Mockito.mockStatic(GeradorDeNumeros);
+    }
+
+
+}
